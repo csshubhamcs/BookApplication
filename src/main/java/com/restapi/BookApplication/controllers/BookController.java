8971dcce71f97769028df73db0a9a73bf283c812
csshubhamcs <shubhamsinghhrx@gmail.com>
2023-04-30 15:44:52 +0530

    .FirstBookApi
    Reason:
    
    Fixes:FirstBookApi

--- src/main/java/com/restapi/BookApplication/controllers/BookController.java
+++ src/main/java/com/restapi/BookApplication/controllers/BookController.java
@@ -1,19 +1,26 @@
 package com.restapi.BookApplication.controllers;
 
 import com.restapi.BookApplication.entities.Book;
+import com.restapi.BookApplication.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class BookController {
+    @Autowired
+    private BookService bookService;
 
     @GetMapping("/books")
-    public Book getBooks(){
+    public List<Book> getBooks(){
 
-        Book book = new Book();
-        book.setId(123);
-        book.setTitle("Complete java");
-        book.setAuthor("Shubham singh");
-        return book;
+
+        return bookService.getAllBooks();
     }
+@GetMapping("/books/{id}")
+    public Book getBookById(@PathVariable("id") int id){
+        return bookService.getBookById(id);
+}
 }
--- src/main/java/com/restapi/BookApplication/services/BookService.java
+++ src/main/java/com/restapi/BookApplication/services/BookService.java
@@ -1,4 +1,27 @@
 package com.restapi.BookApplication.services;
 
+import com.restapi.BookApplication.entities.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookService {
+
+    private static List<Book> list = new ArrayList<>();
+
+    static {
+        list.add(new Book(1,"java1 complete book ","Shubham Singh1"));
+        list.add(new Book(2,"java2 complete book ","Shubham Singh2"));
+        list.add(new Book(3,"java3 complete book ","Shubham Singh3"));
+    }
+
+
+    public List<Book> getAllBooks(){
+
+        return list;
+    }
+
+    public Book getBookById(int id){
+        return list.stream().filter(b -> b.getId() == id).findFirst().get();
+    }
 }
